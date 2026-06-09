a8ab733a1a9459bbe73ecfd397cac638d8416d2b
DESKTOP-TUP056L\udune <udune@naver.com>
2026-06-10 01:06:22 +0900

    feat : Image Size & FPS

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,8 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pictureBoxIpl1 = new System.Windows.Forms.PictureBox();
+            this.cmbSizeMode = new System.Windows.Forms.ComboBox();
+            this.lblMode = new System.Windows.Forms.Label();
+            this.lblInterval = new System.Windows.Forms.Label();
+            this.nudInterval = new System.Windows.Forms.NumericUpDown();
+            this.lblFormula = new System.Windows.Forms.Label();
+            this.lblMeasuredFps = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.renderTimer = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudInterval)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBoxIpl1
@@ -37,26 +49,150 @@
             this.pictureBoxIpl1.Location = new System.Drawing.Point(41, 31);
             this.pictureBoxIpl1.Name = "pictureBoxIpl1";
             this.pictureBoxIpl1.Size = new System.Drawing.Size(635, 368);
+            this.pictureBoxIpl1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBoxIpl1.TabIndex = 0;
             this.pictureBoxIpl1.TabStop = false;
-            this.pictureBoxIpl1.Click += new System.EventHandler(this.pictureBoxIpl1_Click);
+            // 
+            // cmbSizeMode
+            // 
+            this.cmbSizeMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSizeMode.FormattingEnabled = true;
+            this.cmbSizeMode.Location = new System.Drawing.Point(792, 55);
+            this.cmbSizeMode.Name = "cmbSizeMode";
+            this.cmbSizeMode.Size = new System.Drawing.Size(143, 20);
+            this.cmbSizeMode.TabIndex = 1;
+            // 
+            // lblMode
+            // 
+            this.lblMode.AutoSize = true;
+            this.lblMode.Location = new System.Drawing.Point(790, 31);
+            this.lblMode.Name = "lblMode";
+            this.lblMode.Size = new System.Drawing.Size(57, 12);
+            this.lblMode.TabIndex = 2;
+            this.lblMode.Text = "카메라 선택";
+            // 
+            // lblInterval
+            // 
+            this.lblInterval.AutoSize = true;
+            this.lblInterval.Location = new System.Drawing.Point(790, 194);
+            this.lblInterval.Name = "lblInterval";
+            this.lblInterval.Size = new System.Drawing.Size(126, 12);
+            this.lblInterval.TabIndex = 3;
+            this.lblInterval.Text = "Interval (ms, 10~100)";
+            // 
+            // nudInterval
+            // 
+            this.nudInterval.Location = new System.Drawing.Point(792, 217);
+            this.nudInterval.Maximum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.nudInterval.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudInterval.Name = "nudInterval";
+            this.nudInterval.Size = new System.Drawing.Size(143, 21);
+            this.nudInterval.TabIndex = 4;
+            this.nudInterval.Value = new decimal(new int[] {
+            33,
+            0,
+            0,
+            0});
+            this.nudInterval.ValueChanged += new System.EventHandler(this.nudInterval_ValueChanged);
+            // 
+            // lblFormula
+            // 
+            this.lblFormula.AutoSize = true;
+            this.lblFormula.Location = new System.Drawing.Point(790, 252);
+            this.lblFormula.Name = "lblFormula";
+            this.lblFormula.Size = new System.Drawing.Size(29, 12);
+            this.lblFormula.TabIndex = 5;
+            this.lblFormula.Text = "FPS";
+            // 
+            // lblMeasuredFps
+            // 
+            this.lblMeasuredFps.AutoSize = true;
+            this.lblMeasuredFps.Location = new System.Drawing.Point(790, 278);
+            this.lblMeasuredFps.Name = "lblMeasuredFps";
+            this.lblMeasuredFps.Size = new System.Drawing.Size(59, 12);
+            this.lblMeasuredFps.TabIndex = 6;
+            this.lblMeasuredFps.Text = "측정 FPS";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(792, 91);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(143, 32);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "시작";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(792, 130);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(143, 32);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "중지";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(790, 174);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(53, 12);
+            this.lblStatus.TabIndex = 9;
+            this.lblStatus.Text = "상태: 대기";
+            // 
+            // renderTimer
+            // 
+            this.renderTimer.Interval = 33;
+            this.renderTimer.Tick += new System.EventHandler(this.renderTimer_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(964, 432);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblMeasuredFps);
+            this.Controls.Add(this.lblFormula);
+            this.Controls.Add(this.nudInterval);
+            this.Controls.Add(this.lblInterval);
+            this.Controls.Add(this.lblMode);
+            this.Controls.Add(this.cmbSizeMode);
             this.Controls.Add(this.pictureBoxIpl1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "CH02 - 실시간 카메라 캡처";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudInterval)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBoxIpl1;
+        private System.Windows.Forms.ComboBox cmbSizeMode;
+        private System.Windows.Forms.Label lblMode;
+        private System.Windows.Forms.Label lblInterval;
+        private System.Windows.Forms.NumericUpDown nudInterval;
+        private System.Windows.Forms.Label lblFormula;
+        private System.Windows.Forms.Label lblMeasuredFps;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Timer renderTimer;
     }
 }
 
